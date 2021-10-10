00bc98df895f5b76e6402658cb739298759fdbc6
solo5star <solo_5@naver.com>
2021-10-10 17:41:18 +0900

    Serialize Product in ProductSpec.products

--- products/serializers.py
+++ products/serializers.py
@@ -9,15 +9,20 @@ class ProductCategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ProductSpecSerializer(serializers.ModelSerializer):
-    category = ProductCategorySerializer()
-
-    class Meta:
-        model = ProductSpec
-        fields = '__all__'
-
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class ProductSpecSerializer(serializers.ModelSerializer):
+    category = ProductCategorySerializer()
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ProductSpec
+        fields = '__all__'
+        extra_fields = ['products']
+    
+    def get_field_names(self, declared_fields, info):
+        return super(ProductSpecSerializer, self).get_field_names(declared_fields, info) + self.Meta.extra_fields
