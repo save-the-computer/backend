b2d304a702b7c45ab190f8c7bb6f11f5733bf7dd
solo5star <solo_5@naver.com>
2021-11-09 20:42:56 +0900

    celery: wait until djago container ready

--- entrypoints/celery-worker.sh
+++ entrypoints/celery-worker.sh
@@ -2,6 +2,7 @@
 
 /wait-for-it.sh -t 0 $MARIADB_HOST:$MARIADB_PORT
 /wait-for-it.sh -t 0 $RABBITMQ_HOST:$RABBITMQ_PORT
+/wait-for-it.sh -t 0 $DJANGO_HOST:$DJANGO_PORT
 
 # important! last line should be exec, so the process can receive SIGTERM from docker stop.
 exec celery -A products worker --loglevel=info
